e588d4712a714be81b1f55735059fd3279387b17
Arthur Chaloin <arthur.chaloin@epitech.eu>
2018-05-10 15:26:03 +0200

    engine: base display component

---
+++ src/engine/components/DisplayComponent.cpp
@@ -0,0 +1,10 @@
+//
+//  DisplayComponent.cpp
+//  engine
+//
+//  Created by Arthur Chaloin on 10/05/2018.
+//
+
+#include "DisplayComponent.hpp"
+
+engine::DisplayComponent::DisplayComponent()
---
+++ src/engine/components/DisplayComponent.hpp
@@ -0,0 +1,24 @@
+//
+//  DisplayComponent.hpp
+//  engine
+//
+//  Created by Arthur Chaloin on 09/05/2018.
+//
+
+#pragma once
+
+#include <string>
+#include <irrlicht/irrlicht.h>
+#include "Component.hpp"
+
+namespace engine {
+
+	using Node = irr::scene::IAnimatedMeshSceneNode;
+	using MeshNode = irr::scene::IAnimatedMesh;
+
+	struct DisplayComponent : public Component {
+		DisplayComponent(MeshNode* mesh);
+
+		Node* node = nullptr;
+	};
+}
