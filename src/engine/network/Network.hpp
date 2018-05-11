9a3ff17d44f3010a76f406d2a44b31a928f97d86
Arthur Chaloin <arthur.chaloin@epitech.eu>
2018-05-11 13:33:45 +0200

    network: base definitions

---
+++ src/engine/network/Network.hpp
@@ -0,0 +1,36 @@
+//
+//  Network.hpp
+//  worms
+//
+//  Created by Arthur Chaloin on 11/05/2018.
+//
+
+#pragma once
+
+#ifdef WIN32
+
+#include <winsock2.h>
+
+#else
+
+#include <unistd.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#define INVALID_SOCKET -1
+#define SOCKET_ERROR -1
+#define closesocket(s) close(s)
+
+#define NET_PORT			4290
+#define NET_MAX_MSG_SIZE	4096
+#define NET_MAX_CONNECTIONS	2
+
+typedef int SOCKET;
+typedef struct sockaddr_in SOCKADDR_IN;
+typedef struct sockaddr SOCKADDR;
+typedef struct in_addr IN_ADDR;
+
+#endif
