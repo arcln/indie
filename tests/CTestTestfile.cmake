f22f8c9906082b0b60d3429255c9bdb1bc6e3916
thmsrbn <thomas.arbona@epitech.eu>
2018-06-03 20:05:24 +0200

    delete files

--- tests/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/arbona/work/tek/indie/tests
-# Build directory: /home/arbona/work/tek/indie/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
