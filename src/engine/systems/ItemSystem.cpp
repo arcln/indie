434e5e0833b6f4233d8b73afba8612024ea37cb4
thmsrbn <thomas.arbona@epitech.eu>
2018-06-08 18:59:24 +0200

    items: pick/use/throw items

--- src/engine/systems/ItemSystem.cpp
+++ src/engine/systems/ItemSystem.cpp
@@ -4,6 +4,8 @@
 ** File description:
 ** Thomas Arbona
 */
+#include <cmath>
+#include <chrono>
 #include "../core/Entity.hpp"
 #include "ItemSystem.hpp"
 #include "../components/TransformComponent.hpp"
@@ -13,6 +15,7 @@
 void
 engine::ItemSystem::update(Scene& scene)
 {
+    int64_t now = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count() % 1000000;
     Entities& entities = scene.getEntities();
 
     entities.each<TransformComponent>([&](Entity const& e, auto& t) {
@@ -20,7 +23,10 @@ engine::ItemSystem::update(Scene& scene)
 
         if (e.has<ItemComponent>() && parentID == Entity::nullId) {
             t.rotation.Y += 1.f;
-            t.magicPosition.Y = 1.f;
+            t.magicPosition.Y = (0.5f + (std::cos(now / 500.f) / 2.f)) / 2.f;
+        } else {
+            t.rotation.Y = 0.f;
+            t.magicPosition.Y = 0.f;
         }
 
         if (parentID == Entity::nullId)
@@ -32,6 +38,13 @@ engine::ItemSystem::update(Scene& scene)
             auto& t2 = parent.get<TransformComponent>();
             t.position.X = t2.position.X;
             t.position.Y = t2.position.Y;
+
+            if (e.has<ItemComponent>()) {
+                auto& i = e.get<ItemComponent>();
+                t.position.X += i.offset.X;
+                t.position.Y += i.offset.Y;
+                t.position.Z += i.offset.Z;
+            }
         }
     });
 }
